0b44c7a6baaac3c3354796c0fc76f45601a42113
ian rebusa <ianrebusa@gmail.com>
2023-12-11 23:18:35 +0800

    First commit

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-def get_current_weather(city="Kansas City"):
+def get_current_weather(city="Manila"):
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
     weather_data = requests.get(request_url).json()
